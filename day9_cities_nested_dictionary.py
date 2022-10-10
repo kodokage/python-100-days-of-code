dae61730bbc74309494735eea5938a7a6d59a938
J[3~[Coeiaarafini <allaputaafini@gmail.com>
2022-10-10 12:20:00 -0700

    day 9

---
+++ day9_cities_nested_dictionary.py
@@ -0,0 +1,6 @@
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+travel_log["France"] = ({'cities_visited': travel_log["France"]})
+print(travel_log)
